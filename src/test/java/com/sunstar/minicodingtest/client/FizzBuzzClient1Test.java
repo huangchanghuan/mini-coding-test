85a181515b6764dbd9a411f90a367e19a2a96d87
Administrator <changhuan.huang@gmail.com>
2019-03-06 12:35:55 +0800

    update some log

--- src/test/java/com/sunstar/minicodingtest/client/FizzBuzzClient1Test.java
+++ src/test/java/com/sunstar/minicodingtest/client/FizzBuzzClient1Test.java
@@ -10,26 +10,26 @@ public class FizzBuzzClient1Test {
 
     @Test
     public void shouldFizzWhenMultipleOf3() {
-        Assert.assertEquals(FizzBuzzConstant.FIZZ,fizzBuzzClient1.print(3));
-        Assert.assertEquals(FizzBuzzConstant.FIZZ,fizzBuzzClient1.print(66));
+        Assert.assertEquals(FizzBuzzConstant.FIZZ, fizzBuzzClient1.print(3));
+        Assert.assertEquals(FizzBuzzConstant.FIZZ, fizzBuzzClient1.print(66));
     }
 
     @Test
     public void shouldBuzzWhenMultipleOf5() {
-        Assert.assertEquals(FizzBuzzConstant.BUZZ,fizzBuzzClient1.print(5));
-        Assert.assertEquals(FizzBuzzConstant.BUZZ,fizzBuzzClient1.print(100));
+        Assert.assertEquals(FizzBuzzConstant.BUZZ, fizzBuzzClient1.print(5));
+        Assert.assertEquals(FizzBuzzConstant.BUZZ, fizzBuzzClient1.print(100));
     }
 
     @Test
     public void shouldFillBuzzWhenMultipleOf3And5() {
-        Assert.assertEquals(FizzBuzzConstant.FIZZ_BUZZ,fizzBuzzClient1.print(15));
-        Assert.assertEquals(FizzBuzzConstant.FIZZ_BUZZ,fizzBuzzClient1.print(75));
+        Assert.assertEquals(FizzBuzzConstant.FIZZ_BUZZ, fizzBuzzClient1.print(15));
+        Assert.assertEquals(FizzBuzzConstant.FIZZ_BUZZ, fizzBuzzClient1.print(75));
     }
 
     @Test
     public void shouldNumber() {
-        Assert.assertEquals("7",fizzBuzzClient1.print(7));
-        Assert.assertEquals("98",fizzBuzzClient1.print(98));
+        Assert.assertEquals("7", fizzBuzzClient1.print(7));
+        Assert.assertEquals("98", fizzBuzzClient1.print(98));
     }
 
 }
--- src/test/java/com/sunstar/minicodingtest/client/FizzBuzzClient2Test.java
+++ src/test/java/com/sunstar/minicodingtest/client/FizzBuzzClient2Test.java
@@ -10,44 +10,44 @@ public class FizzBuzzClient2Test {
 
     @Test
     public void shouldFizzWhenMultipleOf3() {
-        Assert.assertEquals(FizzBuzzConstant.FIZZ,fizzBuzzClient2.print(3));
-        Assert.assertEquals(FizzBuzzConstant.FIZZ,fizzBuzzClient2.print(66));
+        Assert.assertEquals(FizzBuzzConstant.FIZZ, fizzBuzzClient2.print(3));
+        Assert.assertEquals(FizzBuzzConstant.FIZZ, fizzBuzzClient2.print(66));
     }
 
     @Test
     public void shouldFizzWhenHave3() {
-        Assert.assertEquals(FizzBuzzConstant.FIZZ,fizzBuzzClient2.print(33));
-        Assert.assertEquals(FizzBuzzConstant.FIZZ,fizzBuzzClient2.print(73));
+        Assert.assertEquals(FizzBuzzConstant.FIZZ, fizzBuzzClient2.print(33));
+        Assert.assertEquals(FizzBuzzConstant.FIZZ, fizzBuzzClient2.print(73));
     }
 
     @Test
     public void shouldBuzzWhenMultipleOf5() {
-        Assert.assertEquals( FizzBuzzConstant.BUZZ,fizzBuzzClient2.print(5));
-        Assert.assertEquals( FizzBuzzConstant.BUZZ,fizzBuzzClient2.print(100));
+        Assert.assertEquals(FizzBuzzConstant.BUZZ, fizzBuzzClient2.print(5));
+        Assert.assertEquals(FizzBuzzConstant.BUZZ, fizzBuzzClient2.print(100));
     }
 
     @Test
     public void shouldBuzzWhenHave5() {
-        Assert.assertEquals(FizzBuzzConstant.BUZZ,fizzBuzzClient2.print(56));
-        Assert.assertEquals(FizzBuzzConstant.BUZZ,fizzBuzzClient2.print(59));
+        Assert.assertEquals(FizzBuzzConstant.BUZZ, fizzBuzzClient2.print(56));
+        Assert.assertEquals(FizzBuzzConstant.BUZZ, fizzBuzzClient2.print(59));
     }
 
     @Test
     public void shouldFillBuzzWhenMultipleOf3And5() {
-        Assert.assertEquals(FizzBuzzConstant.FIZZ_BUZZ,fizzBuzzClient2.print(15));
-        Assert.assertEquals(FizzBuzzConstant.FIZZ_BUZZ,fizzBuzzClient2.print(75));
+        Assert.assertEquals(FizzBuzzConstant.FIZZ_BUZZ, fizzBuzzClient2.print(15));
+        Assert.assertEquals(FizzBuzzConstant.FIZZ_BUZZ, fizzBuzzClient2.print(75));
     }
 
     @Test
     public void shouldFizzBuzzWhenHave3And5() {
-        Assert.assertEquals(FizzBuzzConstant.FIZZ_BUZZ,fizzBuzzClient2.print(35));
-        Assert.assertEquals(FizzBuzzConstant.FIZZ_BUZZ,fizzBuzzClient2.print(53));
+        Assert.assertEquals(FizzBuzzConstant.FIZZ_BUZZ, fizzBuzzClient2.print(35));
+        Assert.assertEquals(FizzBuzzConstant.FIZZ_BUZZ, fizzBuzzClient2.print(53));
     }
 
     @Test
     public void shouldNumber() {
-        Assert.assertEquals("7",fizzBuzzClient2.print(7));
-        Assert.assertEquals("98",fizzBuzzClient2.print(98));
+        Assert.assertEquals("7", fizzBuzzClient2.print(7));
+        Assert.assertEquals("98", fizzBuzzClient2.print(98));
     }
 
 }
